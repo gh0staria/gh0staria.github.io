4342a952353ce89ca6d717c4e36b1f0f7cfe7d9b
Liz W <lizwigg6@gmail.com>
2015-05-24 10:27:45 -0500

    Add real projects, delete placeholders

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ function alertViewer() {
 	//this is just a placeholder function
 	alert('You have been alerted!');
 }
-function project1() {
-	document.getElementById('proj-name').innerHTML = 'Project #1';
-	document.getElementById('proj-details').innerHTML = 'This is literally just placeholder text because I\'m still setting all this stuff up.';
-	document.getElementById('proj-link').setAttribute('href', '#');
+function walkingLady() {
+	document.getElementById('proj-name').innerHTML = 'The Walking Lady';
+	document.getElementById('proj-details').innerHTML = 'A simple falling objects game, created to see what I could do with the Phaser game engine.<br><br>Last modified 5/23/15';
+	document.getElementById('proj-link').setAttribute('href', 'http://gh0staria.github.io/the-walking-lady/');
+}
+function newHome () {
+	document.getElementById('proj-name').innerHTML = 'A New Home';
+	document.getElementById('proj-details').innerHTML = 'You just moved in, and now you\'ve started the vaguely heroic quest of unpacking. This story contains an unexciting lack of plot twists, but has ripping open boxes (like an animal), laying on the floor, and even talking to neighbors! If you have absolutley nothing else to do, there are better ways to waste your time.<br><br>Created by gh0staria in like, less than an hour.<br>It\'s that bad.<br><br>Last modifed 5/23/15';
+	document.getElementById('proj-link').setAttribute('href', 'http://gh0staria.github.io/a-new-home/');
+}
+function checkm8 () {
+	document.getElementById('proj-name').innerHTML = 'checkm8';
+	document.getElementById('proj-details').innerHTML = 'Checkm8 is a quick and simple online checklist.<br><br>Last modified 5/23/15';
+	document.getElementById('proj-link').setAttribute('href', 'http://gh0staria.github.io/checkm8/');
 }
