bedae0eeec7c85f4c0469601aae5a7ce25a88e0a
Liz W <lizwigg6@gmail.com>
2015-05-23 23:10:34 -0500

    Show details when sidebar item is selected

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertViewer() {
+	//this is just a placeholder function
+	alert('You have been alerted!');
+}
+function project1() {
+	document.getElementById('proj-name').innerHTML = 'Project #1';
+	document.getElementById('proj-details').innerHTML = 'This is literally just placeholder text because I\'m still setting all this stuff up.';
+	document.getElementById('proj-link').setAttribute('href', '#');
+}
