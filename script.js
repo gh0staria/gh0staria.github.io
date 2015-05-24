9c36ad46484ff2971ce108fce7544dd6013cac22
Liz W <lizwigg6@gmail.com>
2015-05-24 10:58:14 -0500

    Rework innerhtml display of details

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-function alertViewer() {
-	//this is just a placeholder function
-	alert('You have been alerted!');
-}
 function walkingLady() {
-	document.getElementById('proj-name').innerHTML = 'The Walking Lady';
-	document.getElementById('proj-details').innerHTML = 'A simple falling objects game, created to see what I could do with the Phaser game engine.<br><br>Last modified 5/23/15';
-	document.getElementById('proj-link').setAttribute('href', 'http://gh0staria.github.io/the-walking-lady/');
+	document.getElementById('maindiv').innerHTML = '<h3>The Walking Lady</h3><p>A simple falling objects game, created to see what I could do with the Phaser game engine.<br><br>Last modified 5/23/15</p><p><a href="http://gh0staria.github.io/the-walking-lady/">Go to project ➨</a></p>';
 }
-function newHome () {
-	document.getElementById('proj-name').innerHTML = 'A New Home';
-	document.getElementById('proj-details').innerHTML = 'You just moved in, and now you\'ve started the vaguely heroic quest of unpacking. This story contains an unexciting lack of plot twists, but has ripping open boxes (like an animal), laying on the floor, and even talking to neighbors! If you have absolutley nothing else to do, there are better ways to waste your time.<br><br>Created by gh0staria in like, less than an hour.<br>It\'s that bad.<br><br>Last modifed 5/23/15';
-	document.getElementById('proj-link').setAttribute('href', 'http://gh0staria.github.io/a-new-home/');
+function newHome() {
+	document.getElementById('maindiv').innerHTML = '<h3>A New Home</h3><p>You just moved in, and now you\'ve started the vaguely heroic quest of unpacking. This story contains an unexciting lack of plot twists, but has ripping open boxes (like an animal), laying on the floor, and even talking to neighbors! If you have absolutley nothing else to do, there are better ways to waste your time.</p><p>Created by gh0staria in like, less than an hour.<br>It\'s that bad.</p><p>Last modifed 5/23/15</p><p><a href="http://gh0staria.github.io/a-new-home/">Go to project ➨</a></p>';
 }
-function checkm8 () {
-	document.getElementById('proj-name').innerHTML = 'checkm8';
-	document.getElementById('proj-details').innerHTML = 'Checkm8 is a quick and simple online checklist.<br><br>Last modified 5/23/15';
-	document.getElementById('proj-link').setAttribute('href', 'http://gh0staria.github.io/checkm8/');
+function checkm8() {
+	document.getElementById('maindiv').innerHTML = '<h3>checkm8</h3><p>Checkm8 is a quick and simple online checklist.</p><p>Last modified 5/23/15</p><p><a href="http://gh0staria.github.io/checkm8/">Go to project ➨</a></p>';
 }
