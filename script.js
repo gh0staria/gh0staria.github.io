652c8d883e163210b5825baa77d1142fde3fcf5e
Liz W <lizwigg6@gmail.com>
2015-05-24 22:18:36 -0500

    Minor content tweaks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function walkingLady() {
-	document.getElementById('maindiv').innerHTML = '<h3>The Walking Lady</h3><p>A simple falling objects game, created to see what I could do with the Phaser game engine.<br><br>Last modified 5/23/15</p><p><a href="http://gh0staria.github.io/the-walking-lady/">Go to project ➨</a></p>';
+	document.getElementById('maindiv').innerHTML = '<h3><span class="glyphicon glyphicon-wrench"></span> The Walking Lady</h3><p>A simple falling objects game, created to see what I could do with the Phaser game engine.<br><br>Last modified 5/23/15</p><p><a href="http://gh0staria.github.io/the-walking-lady/">Go to project ➨</a></p>';
 }
 function newHome() {
 	document.getElementById('maindiv').innerHTML = '<h3>A New Home</h3><p>You just moved in, and now you\'ve started the vaguely heroic quest of unpacking. This story contains an unexciting lack of plot twists, but has ripping open boxes (like an animal), laying on the floor, and even talking to neighbors! If you have absolutley nothing else to do, there are better ways to waste your time.</p><p>Created by gh0staria in like, less than an hour.<br>It\'s that bad.</p><p>Last modifed 5/23/15</p><p><a href="http://gh0staria.github.io/a-new-home/">Go to project ➨</a></p>';
 }
 function checkm8() {
-	document.getElementById('maindiv').innerHTML = '<h3>checkm8</h3><p>Checkm8 is a quick and simple online checklist.</p><p>Last modified 5/23/15</p><p><a href="http://gh0staria.github.io/checkm8/">Go to project ➨</a></p>';
+	document.getElementById('maindiv').innerHTML = '<h3>checkm8</h3><p>Checkm8 is a quick and simple online checklist.</p><h4>Known Issues</h4><ul><li>Dragging doesn\'t work on mobile</li></ul><p>Last modified 5/23/15</p><p><a href="http://gh0staria.github.io/checkm8/">Go to project ➨</a></p>';
 }
