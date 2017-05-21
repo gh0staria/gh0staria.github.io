33ecb5a5d23d7ac10aa047edd3b4e7f787edb4bb
Liz W <lizwigg6@gmail.com>
2017-05-21 12:34:57 -0500

    Add working masonry filtering system

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -5,41 +5,25 @@ var $grid = $('.grid').masonry({
 	gutter: 10
 });
 
-var items = $grid.masonry('getItemElements');
-
 $grid.imagesLoaded().progress(function () {
 	$grid.masonry('layout');
 });
 
-function filterGrid(fil) {
-	$grid.masonry('reloadItems');
-	$grid.masonry('layout');
-	
-	var	revealItems = [],
-		hideItems = [];
-
-	$.each(items, function (i) {
-		var item = items[i],
-			shouldShow = item.dataset.filter === fil || fil === "";
-
-		if (shouldShow) {
-			item.isIgnored = false;
-			item.isHidden = false;
-			revealItems.push(item);
-		} else {
-			item.isIgnored = true;
-			item.isHidden = true;
-			hideItems.push(item);
-		}
-	});
-	
-	$grid.masonry('remove', hideItems);
-	$grid.append(revealItems);
-	$grid.masonry('appended', revealItems).masonry('layout');
-}
-
 $(".filters-btn").click(function (e) {
-	var filter = $(this).attr("data-filter");
+	// filtering code from http://www.epicwebs.co.uk/jquery-tutorials/quick-and-easy-jquery-masonry-tutorial/
 	
-	filterGrid(filter);
+	$(".filters-btn").removeClass("active");
+	$(this).addClass("active");
+	
+	var group = $(this).attr("data-filter");
+	var groupClass = "." + group;
+	
+	if(group != "") {
+		$(".grid-item").hide();
+		$(groupClass).show();
+		$('.grid').masonry('layout');
+	} else {
+		$(".grid-item").show();
+		$('.grid').masonry('layout');
+	}
 });
