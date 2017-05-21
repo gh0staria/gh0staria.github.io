797bdeb32f91c4c374d63def74d7893ad89b38bf
Liz W <lizwigg6@gmail.com>
2017-05-20 20:22:07 -0500

    Add working filtering system

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,21 +1,45 @@
-/*function showAll() {
-	$(".grid-item").css("display", "block");
-}
-
-function showCategory(cat) {
-	$(".grid-item").hide();
-	$("." + cat).css("display", "block");
-	salvattore.rescanMediaQueries();
-}*/
-
 var $grid = $('.grid').masonry({
-	// options
 	itemSelector: '.grid-item',
 	columnWidth: '.grid-sizer',
 	percentPosition: true,
 	gutter: 10
 });
 
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry('layout');
+var items = $grid.masonry('getItemElements');
+
+$grid.imagesLoaded().progress(function () {
+	$grid.masonry('layout');
+});
+
+function filterGrid(fil) {
+	$grid.masonry('reloadItems');
+	$grid.masonry('layout');
+	
+	var	revealItems = [],
+		hideItems = [];
+
+	$.each(items, function (i) {
+		var item = items[i],
+			shouldShow = item.dataset.filter === fil || fil === "";
+
+		if (shouldShow) {
+			item.isIgnored = false;
+			item.isHidden = false;
+			revealItems.push(item);
+		} else {
+			item.isIgnored = true;
+			item.isHidden = true;
+			hideItems.push(item);
+		}
+	});
+	
+	$grid.masonry('remove', hideItems);
+	$grid.append(revealItems);
+	$grid.masonry('appended', revealItems).masonry('layout');
+}
+
+$(".filters-btn").click(function (e) {
+	var filter = $(this).attr("data-filter");
+	
+	filterGrid(filter);
 });
