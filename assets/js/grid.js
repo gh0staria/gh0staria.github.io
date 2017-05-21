ee882aab8723b1814785b4138b16846492b91197
Liz W <lizwigg6@gmail.com>
2017-05-21 13:43:42 -0500

    Make portfolio page prettier

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -2,7 +2,8 @@ var $grid = $('.grid').masonry({
 	itemSelector: '.grid-item',
 	columnWidth: '.grid-sizer',
 	percentPosition: true,
-	gutter: 10
+	gutter: 10,
+	stagger: 5
 });
 
 $grid.imagesLoaded().progress(function () {
@@ -12,8 +13,8 @@ $grid.imagesLoaded().progress(function () {
 $(".filters-btn").click(function (e) {
 	// filtering code from http://www.epicwebs.co.uk/jquery-tutorials/quick-and-easy-jquery-masonry-tutorial/
 	
-	$(".filters-btn").removeClass("active");
-	$(this).addClass("active");
+	$("button").removeClass("active");
+	$(this).toggleClass("active");
 	
 	var group = $(this).attr("data-filter");
 	var groupClass = "." + group;
