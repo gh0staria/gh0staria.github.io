f7a1c551c02fc6b91b36fefaa7ae9a0e41bae984
Liz Wigglesworth <lizwigg6@gmail.com>
2018-10-13 20:04:07 -0500

    Add basic filtering functionality

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", initialize);
+
 var Shuffle = window.Shuffle;
 var element = document.querySelector('.project-grid');
 var sizer = element.querySelector('.sizer');
@@ -6,3 +8,16 @@ var shuffleInstance = new Shuffle(element, {
     itemSelector: '.portfolio-item',
     sizer: sizer
 });
+
+function initialize() {
+    document.getElementById("codefilter").onclick = filterGrid;
+    document.getElementById("designfilter").onclick = filterGrid;
+}
+
+function filterGrid(event)
+{
+    var filterby = event.target.innerText; // get text of button
+    console.log(filterby);
+
+    shuffleInstance.filter(filterby);
+}
